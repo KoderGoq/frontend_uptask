6734d0bc77197c61f9e47c2cc773d008f3f9d91c
Fernando Aguilar <100661177+KoderGoq@users.noreply.github.com>
2025-06-20 23:15:16 -0600

    Page 404

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -7,7 +7,7 @@ const AuthLayout = () => {
     <>
       <div className='bg-gray-800 min-h-screen'>
         <div className='py-10 lg:py-20 mx-auto w-[450px]'>
-          <Link to={'/auth/login'}>
+          <Link to={'/'}>
             <Logo />
           </Link>
           <div className='mt-10'>
